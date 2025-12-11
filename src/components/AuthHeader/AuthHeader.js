98f63810869f6dcc6c4070474b5df240249e0b30
Ravish <ravish012@gmail.com>
2025-12-11 21:06:44 +0530

    Update AuthHeader.js

--- src/components/AuthHeader/AuthHeader.js
+++ src/components/AuthHeader/AuthHeader.js
@@ -5,9 +5,20 @@ export default function AuthHeader() {
     return (
         <AppBar position="sticky" color="default" elevation={1} sx={{ top: 0, zIndex: 1100 }}>
             <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
-                    ApartmentTodos
-                </Typography>
+                <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
+                    <Box
+                        component="img"
+                        src="/logo.png"
+                        alt="SITARA Logo"
+                        sx={{
+                            height: 50,
+                            mr: 2
+                        }}
+                    />
+                    <Typography variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
+                        SITARA
+                    </Typography>
+                </Box>
             </Toolbar>
         </AppBar>
     )
